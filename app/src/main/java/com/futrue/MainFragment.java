768d0332a49441d5110ae315ed9a5b60e1f9fedd
William Shakespeare <saint.half@outlook.com>
2019-04-23 11:35:49 +0800

    test

---
+++ app/src/main/java/com/futrue/MainFragment.java
@@ -0,0 +1,8 @@
+package com.futrue;
+
+import android.support.v4.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+
+}
---
+++ app/src/main/java/com/futrue/view/ProgressView.java
@@ -0,0 +1,13 @@
+package com.futrue.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ProgressView extends View {
+
+    public ProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
